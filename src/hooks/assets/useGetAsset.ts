c7f9f980ebcd0fa13cfb5402a5333189004dc690
mikiiiiihara <kimity0824@icloud.com>
2022-11-25 17:35:20 +0900

    useGetAssetインポート修正

--- src/hooks/assets/useGetAsset.ts
+++ src/hooks/assets/useGetAsset.ts
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import { Asset } from "../../common/asset.interface";
+import { Asset } from "../../types/asset.type";
 
 export const GET_ASSETS = gql`
   query GetAssets($user: String) {
@@ -34,7 +34,9 @@ const GET_ASSET = gql`
     }
   }
 `;
-export const UseGetAsset = (loginUser:string | null | undefined): Asset | undefined => {
-  const { data } = useQuery(GET_ASSET, {variables: {user: loginUser}});
+export const UseGetAsset = (
+  loginUser: string | null | undefined
+): Asset | undefined => {
+  const { data } = useQuery(GET_ASSET, { variables: { user: loginUser } });
   return data?.readAsset;
 };
