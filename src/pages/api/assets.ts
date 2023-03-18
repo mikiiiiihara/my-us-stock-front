c5eec3f96df091d563fba238595ed15560c9c4a6
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-03-19 06:06:21 +0900

    cronを設定

---
+++ src/pages/api/assets.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  result: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const result = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/assets`, {
+    user: "mikiwhigh1274@gmail.com",
+  });
+  console.log(result.data);
+  res.status(200).json({ result: result.data });
+}
