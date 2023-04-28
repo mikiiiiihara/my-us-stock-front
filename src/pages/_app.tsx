d2ea4f4dde8b0ed077a37b1a22490058eb3b13a0
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-04-29 03:11:34 +0800

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
+  console.log(`${process.env.NEXT_PUBLIC_API_URL}/auth`);
   return <Component {...pageProps} />;
 }
 export default App;
