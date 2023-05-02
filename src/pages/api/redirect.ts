06cf996cbd68dc9dab82e9ecf8e4acca22482ceb
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-03 03:09:33 +0900

    Update redirect.ts

--- src/pages/api/redirect.ts
+++ src/pages/api/redirect.ts
@@ -1,51 +1,20 @@
-import fetch from "isomorphic-unfetch";
+// pages/api/auth/redirect.ts
 import { NextApiRequest, NextApiResponse } from "next";
-import safeStringify from "fast-safe-stringify";
 
-const googleAuthRedirect = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
+async function redirect(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // Filter out problematic headers
-    const filteredHeaders = Object.fromEntries(
-      Object.entries(req.headers).filter(
-        ([key, value]) => key.toLowerCase() !== "set-cookie"
-      )
-    );
-    const response = await fetch(
-      "https://my-us-stock-km5gk6oanq-an.a.run.app/auth/redirect",
-      {
-        headers: {
-          ...filteredHeaders,
-          "Content-Type": "application/json",
-        },
-        redirect: "manual",
-        credentials: "include",
-      }
-    );
-
-    if (!response.ok && response.status !== 302) {
-      throw new Error(`Request failed with status code ${response.status}`);
+    // NestJSから設定されたCookieが存在する場合、それを使用
+    if (req.headers["set-cookie"]) {
+      res.setHeader("Set-Cookie", req.headers["set-cookie"]);
     }
 
-    // Get cookies from the response
-    const cookies = response.headers.get("set-cookie");
-
-    if (cookies) {
-      // Set cookies on the client side
-      res.setHeader("Set-Cookie", cookies);
-    }
-
-    // Redirect to a success page or home page
-    res.writeHead(302, {
-      Location: "/",
-    });
+    // リダイレクト先を設定（例: ホームページ）
+    res.writeHead(302, { Location: "/" });
     res.end();
-  } catch (error: any) {
+  } catch (error) {
     console.error(error);
-    res.status(500).json({ message: safeStringify(error) });
+    res.status(500).json({ error: "Internal Server Error" });
   }
-};
+}
 
-export default googleAuthRedirect;
+export default redirect;
