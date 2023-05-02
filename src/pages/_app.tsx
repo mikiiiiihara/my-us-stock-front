081be680eec5c5182fbcc38016ffb3f6711ba2e5
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-03 03:38:24 +0900

    不要なconsole.log削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
-  console.log(`${process.env.NEXT_PUBLIC_API_URL}/auth`);
   return <Component {...pageProps} />;
 }
 export default App;
