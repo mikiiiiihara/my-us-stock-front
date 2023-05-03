6c3c6614a5ddcb25da9ccd8ec2e38c240b197492
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-03 19:11:54 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const cookies = parse(context.req.headers.cookie || "");
   const accessToken = cookies["accessToken"] || null;
   console.log("serverside:");
-  console.log(context.req.headers);
+  console.log(context.req.headers.cookie);
   return {
     props: {
       accessToken,
