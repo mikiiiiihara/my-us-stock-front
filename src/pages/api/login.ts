30d30b7eda4c4a52eda9dccb3e7c97e9aae1e519
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-21 03:35:27 +0900

    Update login.ts

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,22 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_API_URL}/auth`,
-      {
-        headers: req.headers,
-      }
-    );
-
     // Redirect to Google's login page
     res.writeHead(302, {
-      Location: response.request.res.responseUrl,
+      Location: `${process.env.NEXT_PUBLIC_API_URL}/auth`,
     });
     res.end();
   } catch (error) {
