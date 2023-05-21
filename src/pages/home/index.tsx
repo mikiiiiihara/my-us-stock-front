49f97cebe67d3f63fdad42bb36ebf58c9e542e20
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-22 03:19:15 +0900

    リダイレクト時リロードするよう修正

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
 import { HomeContent } from "../../contents/home/homeContent";
 import { NextHead } from "../../components/common/next-head/nextHead";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // パラメータredirectedがtrueならページをリロードする
+    if (router.query.redirected === "true") {
+      // パラメータを削除する
+      router.replace(router.pathname);
+    }
+  }, [router, router.query]);
   return (
     <>
       <NextHead title="My US Stock Portfolio | Home" />
