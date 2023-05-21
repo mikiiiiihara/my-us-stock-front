0f36377015a24370adc5fd29a413734a2dc6cc2b
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-22 03:41:45 +0900

    リフレッシュ後のリロードうまくいかないので一旦戻す

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,16 @@
 import { HomeContent } from "../../contents/home/homeContent";
 import { NextHead } from "../../components/common/next-head/nextHead";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const Home = () => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    // パラメータredirectedがtrueならページをリロードする
-    if (router.query.redirected === "true") {
-      // パラメータを削除する
-      router.replace(router.pathname);
-    }
-  }, [router, router.query]);
+  // useEffect(() => {
+  //   // パラメータredirectedがtrueならページをリロードする
+  //   if (router.query.redirected === "true") {
+  //     // パラメータを削除する
+  //     router.replace(router.pathname);
+  //   }
+  // }, [router, router.query]);
   return (
     <>
       <NextHead title="My US Stock Portfolio | Home" />
